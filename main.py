27510b87db9d1c30825244be5a4a2760d8fe2698
Honoka55 <1915637207@qq.com>
2023-11-12 19:31:23 +0800

    finish text pretrain

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import torch
 import pickle
 import csv
 
+import pickle
+
+
+def read_pickle_and_print(path):
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+        print(data)
+
 # 加载预训练的BERT模型和分词器
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained('bert-base-uncased')
@@ -14,15 +22,31 @@ with open('entity_description.txt', 'r', encoding='utf-8') as f:
     for row in reader:
         data.append(row[1])
 
-# 对文本进行分词和编码
-encoded_data = tokenizer(data, padding=True, truncation=True, return_tensors='pt')
+# 逐批次进行分词和编码
+batch_size = 16  # 根据内存情况调整批次大小
+text_features_list = []
 
-# 使用BERT模型获取文本特征
-with torch.no_grad():
-    outputs = model(**encoded_data)
+for i in range(0, len(data), batch_size):
+    print(i);
 
-# 提取最后一层隐藏状态作为文本特征
-text_features = outputs.last_hidden_state
+    batch_data = data[i:i + batch_size]
+
+    # 对文本进行分词和编码
+    encoded_data = tokenizer(batch_data, padding=True, truncation=True, return_tensors='pt')
+
+    # 使用BERT模型获取文本特征
+    with torch.no_grad():
+        outputs = model(**encoded_data)
+
+    # 提取最后一层隐藏状态作为文本特征
+    text_features = outputs.last_hidden_state
+    text_features_list.append(text_features)
+
+    # 清理GPU缓存
+    torch.cuda.empty_cache()
+
+# 合并所有批次的文本特征
+text_features = torch.cat(text_features_list, dim=0)
 
 # 将文本特征保存为pickle文件
 with open('text_features.pkl', 'wb') as f:
